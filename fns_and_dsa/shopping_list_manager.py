db99d102b848a6ba7ea92c870d816ddd1a1451fc
Nicole Nereah <nicole@learning.git>
2025-09-25 12:53:59 +0300

    created main.py and shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,49 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list =[]
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input('Enter the item name to add: ')
+            if item:
+                shopping_list.append(item)
+                print(f"'{item}' has been added to the shopping list.")
+                print()
+            else:
+                print("No item has been added to the list")  
+        elif choice == '2':
+            if not shopping_list:
+                print("The is nothing to remove")
+                continue
+            item = input("What would you want to remove?: ")
+            if not item:
+                print('No item entered')
+                continue    
+            if item in shopping_list:
+                shopping_list.remove(item)    
+                print(f"'{item}' has been removed from the shopping list.") 
+            else:
+                print(f"Item '{item}' not found in the shopping list.") 
+        elif choice == '3':
+            if not shopping_list:
+                print("The list is empty")
+            else:
+                print("Current List:")
+                for index, value in enumerate(shopping_list):
+                    print(f"{index}. {value}")
+        elif choice == '4':
+            print('Goodbye!')
+            break
+        else:
+            print('invalid choice entered')
+
+if __name__ == '__main__':
+    main()                                              
